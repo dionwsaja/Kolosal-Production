676ae928704bda49e0efab72e321c461d22c2332
dionwsaja <dionwsaja@gmail.com>
2026-02-17 16:58:40 +0700

    commit

--- src/sections/Home/Demo.tsx
+++ src/sections/Home/Demo.tsx
@@ -47,7 +47,7 @@ export function Demo() {
             field experience across national-scale projects, we craft visuals
             that don’t just look good, but represent identity with purpose.
           </p>
-          <div className="mt-10 flex flex-col md:flex-row gap-4 w-auto">
+          <div className="mt-10 flex flex-col md:flex-row gap-4 w-full md:w-auto">
             <Link
               to="/services"
             >
